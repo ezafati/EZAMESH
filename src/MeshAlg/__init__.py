3db6e87796be01b2268f5b0811d3896ced306761
ezafati <padawan@padawan>
2019-12-10 17:13:46 +0100

    delete DT_prim.py

--- src/MeshAlg/__init__.py
+++ src/MeshAlg/__init__.py
@@ -1,2 +1,3 @@
-from . import DT_prim, insert_point_algorithms, initial_DT
-__all__ = [DT_prim, insert_point_algorithms, initial_DT]
+from . import insert_point_algorithms, initial_DT
+
+__all__ = [insert_point_algorithms, initial_DT]
